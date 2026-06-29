61024824d56f06191ac87f88f1cf3392680ca19d
Lucas Rafagnin <lucas_ulrich_foz@hotmail.com>
2026-06-29 00:09:10 -0300

    change Name

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 ## P2 CSI-22 -- Catalogo de Prestadores de Servico TI
 ## Pedro Henrique Moreira
-## Lucas Ulrich
+## Lucas Ulrich Rafagnin
 ## ====================================================
 
 ## interface grafica que vai rodar a aplicacao como um todo
